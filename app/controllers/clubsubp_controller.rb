86db12bbdd1336982e1021b9d97d6ec3299c9e39
Christina Wassermann <christina@tuebingen.mpg.de>
2010-02-11 11:05:57 +0100

    ClubSub: Insertion of the Tabs for the database and tool infrastructure

--- app/controllers/clubsubp_controller.rb
+++ app/controllers/clubsubp_controller.rb
@@ -1,6 +1,22 @@
 class ClubsubpController < ToolController
 
-	def index
-	end
-  
+  def index
+    getlinks
+  end
+
+  def genomes
+    getlinks
+  end
+
+  def search
+    getlinks
+  end
+
+  def getlinks
+    @shortlinks = ["in", "ge", "se"]
+    @links = ["index", "genomes", "search"]
+    @link_names = ["Description", "Browse Genomes", "Search Genomes"]
+    @link_text = ["Description", "Browse the genomes", "Search the genomes"]
+  end
+
 end
