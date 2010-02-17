922540c610be791c618049eadfd629caa6ff6f35
Nagarajan Paramasivam <nagarajan@nolt.eb.local>
2010-02-17 17:09:47 +0100

    ClubSub-P: Updates of Cluster.rhtml, Protein.rhtml

--- app/models/clubsubp/clubsubp_action.rb
+++ app/models/clubsubp/clubsubp_action.rb
@@ -1,9 +1,17 @@
 class ClubsubpAction < Action
 
+  CLUB = File.join(BIOPROGS, 'clubsubp')
 
   # Put action initialisation code in here
-  # def before_perform
-  # end
+  def before_perform
+    init
+
+    @infile = @basename+".in"
+    params_to_file(@infile, 'sequence_input', 'sequence_file')
+    @outfile = @basename+".out"
+    logger.debug "Outfile: #{@outfile}"
+
+  end
 
 
   # Optional:
@@ -13,8 +21,19 @@ class ClubsubpAction < Action
   
   
   # Put action code in here
-  # def perform
-  # end
+  def perform
+    @commands << "/usr/bin/perl #{CLUB}/blast_search.pl #{@infile} #{@outfile} &> #{job.statuslog_path}"
+
+    logger.debug "Commands:\n"+@commands.join("\n")
+    queue.submit(@commands)
+  end
+
+  
+  def init
+    @basename = File.join(job.job_dir, job.jobid)
+    @commands = []
+  end
+
 
 end
 
--- public/javascripts/clubsubp.js
+++ public/javascripts/clubsubp.js
@@ -1,1 +1,23 @@
-// Put your javascript code here!
+function toggle_ali(){
+
+        if( $('aln_file').value=="Hide alignment" ){
+                Effect.Fade('aln');
+                $('aln_file').value="Show alignment";
+        }else{
+                Effect.Appear('aln');
+                $('aln_file').value="Hide alignment";
+        }
+}
+
+
+function toggle_sp_table(){
+
+        if( $('sp_table').value=="Hide SP_table" ){
+                Effect.Fade('sp');
+                $('sp_table').value="Show SP_table";
+        }else{
+                Effect.Appear('sp');
+                $('sp_table').value="Hide SP_table";
+        }
+}
+
