6d42077a52204aba8e88fdad37e8c8fabcbf64e2
Nagarajan Paramasivam <nagarajan@nolt.eb.local>
2010-02-24 10:49:43 +0100

    ClubsubP: Updated the keyword search in the search page

--- app/models/clubsubp/clubsubp_action.rb
+++ app/models/clubsubp/clubsubp_action.rb
@@ -2,7 +2,7 @@ class ClubsubpAction < Action
 
   BLAST  = File.join(BIOPROGS, 'blast')
   DBPATH = File.join(DATABASES, 'clubsubp', 'clst_m_scl')
-  PARSER = File.join(BIOPROGS, 'clubsubp')
+  CLUB   = File.join(BIOPROGS, 'clubsubp')
 
 
   attr_accessor :mail, :jobid, :sequence_input, :sequence_file
@@ -11,11 +11,16 @@ class ClubsubpAction < Action
   def before_perform
     init
 
+    @text_search = params['text_search'] ? params['text_search'] : ""
+
     @infile = @basename+".in"
     params_to_file(@infile, 'sequence_input', 'sequence_file')
     @outfile = @basename+".out"
     logger.debug "Outfile: #{@outfile}"
 
+    if(!@text_search.empty?)
+      @infile=""
+    end  
   end
 
 
@@ -27,13 +32,30 @@ class ClubsubpAction < Action
   
   # Put action code in here
   def perform
-    @commands << "#{BLAST}/blastall -p blastp -i #{@infile} -d #{DBPATH} -o #{@outfile} -I t &>#{job.statuslog_path}"
-    @commands << "echo 'Finished BLAST search!' >> #{job.statuslog_path}"
-    @commands << "/usr/bin/perl #{PARSER}/blast_parser.pl #{@outfile} #{@basename} >> #{job.statuslog_path}"
-    @commands << "echo 'Blast ouput parsed !' >> #{job.statuslog_path}"
 
+    # Text search starts here
+    if(!@text_search.empty?)
+      @commands << "echo #{@basename} &> #{job.statuslog_path}"
+      @commands << "/usr/bin/perl #{CLUB}/search_clubsub.pl #{@basename} #{@text_search} >> #{job.statuslog_path}" 
+      @commands << "echo #{@text_search} >> #{job.statuslog_path}"
+    end
+
+    # Blast search if we have sequence input 
+    if(!@infile.empty?)
+      @commands << "#{BLAST}/blastall -p blastp -i #{@infile} -d #{DBPATH} -o #{@outfile} -I t &>#{job.statuslog_path}"
+      @commands << "echo 'Finished BLAST search!' >> #{job.statuslog_path}"
+      @commands << "/usr/bin/perl #{CLUB}/blast_parser.pl #{@outfile} #{@basename} >> #{job.statuslog_path}"
+      @commands << "echo 'Blast output parsed !' >> #{job.statuslog_path}"
+
+    end
+   
+    if(@infile.empty? && @text_search.empty?)
+      @commands << "echo 'no input data' &> #{job.statuslog_path}"
+    end 
+ 
     logger.debug "Commands:\n"+@commands.join("\n")
     queue.submit(@commands)
+
   end
 
   
