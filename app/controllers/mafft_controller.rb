d097654c52f79261fcc9d3cf8cb90b151a428a01
Christina Wassermann <christina@tuebingen.mpg.de>
2010-07-21 09:23:33 +0200

    Release 2.12.5
    - include forward to cs_blast in all tools where forward to psi_blast already exists.

--- app/controllers/mafft_controller.rb
+++ app/controllers/mafft_controller.rb
@@ -7,20 +7,20 @@ class MafftController < ToolController
     @fullscreen = true
     @fw_values = [fw_to_tool_url('mafft', 'ali2d'), fw_to_tool_url('mafft', 'alnviz'),
                   fw_to_tool_url('mafft', 'ancescon'), fw_to_tool_url('mafft', 'blastclust'),
-                  fw_to_tool_url('mafft', 'clans'), fw_to_tool_url('mafft', 'seq2gi'),
-                  fw_to_tool_url('mafft', 'fast_hmmer'), fw_to_tool_url('mafft', 'frpred'),
-                  fw_to_tool_url('mafft', 'hhpred'), fw_to_tool_url('mafft', 'hhsenser'),
-                  fw_to_tool_url('mafft', 'psi_blast'), fw_to_tool_url('mafft', 'phylip'),
-                  fw_to_tool_url('mafft', 'quick2_d'), fw_to_tool_url('mafft', 'reformat'),
-                  fw_to_tool_url('mafft', 'repper')]
+                  fw_to_tool_url('mafft', 'clans'), fw_to_tool_url('mafft', 'cs_blast'),
+                  fw_to_tool_url('mafft', 'seq2gi'), fw_to_tool_url('mafft', 'fast_hmmer'),
+                  fw_to_tool_url('mafft', 'frpred'), fw_to_tool_url('mafft', 'hhpred'),
+                  fw_to_tool_url('mafft', 'hhsenser'), fw_to_tool_url('mafft', 'psi_blast'),
+                  fw_to_tool_url('mafft', 'phylip'), fw_to_tool_url('mafft', 'quick2_d'),
+                  fw_to_tool_url('mafft', 'reformat'), fw_to_tool_url('mafft', 'repper')]
     @fw_labels = [tool_title('ali2d'), tool_title('alnviz'),
                   tool_title('ancescon'), tool_title('blastclust'),
-                  tool_title('clans'), tool_title('seq2gi'),
-                  tool_title('fast_hmmer'), tool_title('frpred'),
-                  tool_title('hhpred'), tool_title('hhsenser'),
-                  tool_title('phylip'), tool_title('psi_blast'),
-                  tool_title('quick2_d'), tool_title('reformat'),
-                  tool_title('repper')]
+                  tool_title('clans'), tool_title('cs_blast'),
+                  tool_title('seq2gi'), tool_title('fast_hmmer'),
+                  tool_title('frpred'), tool_title('hhpred'),
+                  tool_title('hhsenser'), tool_title('phylip'),
+                  tool_title('psi_blast'), tool_title('quick2_d'),
+                  tool_title('reformat'), tool_title('repper')]
 
   end
 
--- app/controllers/psi_blast_controller.rb
+++ app/controllers/psi_blast_controller.rb
@@ -23,52 +23,54 @@ class PsiBlastController < ToolController
   end
 
   def results
-    # mode: 
+    # mode:
     #   -alignment: use alignment of alignhits.pl
     #   -seqs: use sequence by hsp or complete sequence
-    @fw_values = [fw_to_tool_url('psi_blast', 'blammer'), 
-                  fw_to_tool_url('psi_blast', 'blastclust') + "&fw_mode=seqs", 
-                  fw_to_tool_url('psi_blast', 'clans') + "&fw_mode=seqs", 
-                  fw_to_tool_url('psi_blast', 'clustalw') + "&fw_mode=seqs", 
-                  fw_to_tool_url('psi_blast', 'seq2gi') + "&fw_mode=alignment",
-                  fw_to_tool_url('psi_blast', 'frpred') + "&fw_mode=alignment",                  
+    @fw_values = [fw_to_tool_url('psi_blast', 'blammer'),
+                  fw_to_tool_url('psi_blast', 'blastclust') + "&fw_mode=seqs",
+                  fw_to_tool_url('psi_blast', 'clans') + "&fw_mode=seqs",
+                  fw_to_tool_url('psi_blast', 'clustalw') + "&fw_mode=seqs",
+                  fw_to_tool_url('psi_blast', 'cs_blast') + "&fw_mode=alignment",
+		  fw_to_tool_url('psi_blast', 'seq2gi') + "&fw_mode=alignment",
+                  fw_to_tool_url('psi_blast', 'frpred') + "&fw_mode=alignment",
                   fw_to_tool_url('psi_blast', 'hhblits') + "&fw_mode=alignment",
                   fw_to_tool_url('psi_blast', 'hhpred') + "&fw_mode=alignment",
-                  fw_to_tool_url('psi_blast', 'hhsenser') + "&fw_mode=alignment", 
-                  fw_to_tool_url('psi_blast', 'kalign') + "&fw_mode=seqs", 
-                  fw_to_tool_url('psi_blast', 'mafft') + "&fw_mode=seqs", 
+                  fw_to_tool_url('psi_blast', 'hhsenser') + "&fw_mode=alignment",
+                  fw_to_tool_url('psi_blast', 'kalign') + "&fw_mode=seqs",
+                  fw_to_tool_url('psi_blast', 'mafft') + "&fw_mode=seqs",
                   fw_to_tool_url('psi_blast', 'muscle') + "&fw_mode=seqs",
 		  fw_to_tool_url('psi_blast', 'patsearch') + "&fw_mode=seqs",
     		  fw_to_tool_url('psi_blast', 'probcons') + "&fw_mode=seqs",
-                  fw_to_tool_url('psi_blast', 'psi_blast') + "&fw_mode=alignment", 
-                  fw_to_tool_url('psi_blast', 'quick2_d') + "&fw_mode=alignment",                   
-                  fw_to_tool_url('psi_blast', 'reformat') + "&fw_mode=alignment",                   
+                  fw_to_tool_url('psi_blast', 'psi_blast') + "&fw_mode=alignment",
+                  fw_to_tool_url('psi_blast', 'quick2_d') + "&fw_mode=alignment",
+                  fw_to_tool_url('psi_blast', 'reformat') + "&fw_mode=alignment",
                   fw_to_tool_url('psi_blast', 'repper') + "&fw_mode=alignment"]
-                  
-    @fw_labels = [tool_title('blammer'), tool_title('blastclust'), tool_title('clans'), 
-                  tool_title('clustalw'), tool_title('seq2gi'), tool_title('frpred'),
-                  tool_title('hhblits'), tool_title('hhpred'),
-                  tool_title('hhsenser'), tool_title('kalign'), tool_title('mafft'), 
-                  tool_title('muscle'), tool_title('patsearch'), tool_title('probcons'), 
-                  tool_title('psi_blast'), tool_title('quick2_d'), tool_title('reformat'), 
+
+    @fw_labels = [tool_title('blammer'), tool_title('blastclust'), tool_title('clans'),
+                  tool_title('clustalw'), tool_title('cs_blast'), tool_title('seq2gi'),
+		  tool_title('frpred'), tool_title('hhblits'), tool_title('hhpred'),
+                  tool_title('hhsenser'), tool_title('kalign'), tool_title('mafft'),
+                  tool_title('muscle'), tool_title('patsearch'), tool_title('probcons'),
+                  tool_title('psi_blast'), tool_title('quick2_d'), tool_title('reformat'),
                   tool_title('repper')]
-                      					
+
   end
-  
+
   def results_alignment
     # mode aignment: use alignment of alignhits.pl
     @fw_values = [fw_to_tool_url('psi_blast', 'blastclust'),
                   fw_to_tool_url('psi_blast', 'clans'),
-                  fw_to_tool_url('psi_blast', 'seq2gi'),
+                  fw_to_tool_url('psi_blast', 'psi_blast'),
+		  fw_to_tool_url('psi_blast', 'seq2gi'),
                   fw_to_tool_url('psi_blast', 'hhpred'),
                   fw_to_tool_url('psi_blast', 'hhsenser'),
                   fw_to_tool_url('psi_blast', 'psi_blast'),
                   fw_to_tool_url('psi_blast', 'reformat'),
                   fw_to_tool_url('psi_blast', 'repper')]
-                  
-    @fw_labels = [tool_title('blastclust'), tool_title('clans'), tool_title('seq2gi'), 
-                  tool_title('hhpred'), tool_title('hhsenser'),  tool_title('psi_blast'), 
-                  tool_title('reformat'), tool_title('repper')]
+
+    @fw_labels = [tool_title('blastclust'), tool_title('clans'), tool_title('cs_blast'),
+		  tool_title('seq2gi'), tool_title('hhpred'), tool_title('hhsenser'),
+		  tool_title('psi_blast'), tool_title('reformat'), tool_title('repper')]
   end
 
   def results_hitlist
