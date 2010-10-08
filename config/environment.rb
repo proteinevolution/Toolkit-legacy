e9cb2ad5c8097ac3195eed5eae320f1bc2cd1a90
Toolkit manager <toolkitmgr@hn01.cluster.local>
2010-10-08 12:19:55 +0200

    Create release 2.13.11

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ memcache_options = {
 memcache_servers = [ '127.0.0.1:11211' ]
 
 #global settings
-TOOLKIT_RELEASE = '2.13.10'
+TOOLKIT_RELEASE = '2.13.11'
 DOC_ROOTURL     = 'http://' + DOC_ROOTHOST
 BIOPROGS        = TOOLKIT_ROOT+'/bioprogs'
 TMP             = TOOLKIT_ROOT+'/tmp/'+RAILS_ENV
