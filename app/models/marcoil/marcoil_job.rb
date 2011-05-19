44d7dc873468ed2a49d3a78a527145832a413d86
Marialke <marialke@genzentrum.lmu.de>
2011-05-19 14:38:28 +0200

    Added Marcoil to the library of working tools

---
+++ app/models/marcoil/marcoil_job.rb
@@ -0,0 +1,66 @@
+class MarcoilJob < Job
+
+  attr_reader :psipred, :coils, :alignment, :pssm
+
+  @@export_ext = ".numerical"
+  def set_export_ext(val)
+    @@export_ext = val
+  end
+  def get_export_ext
+    @@export_ext
+  end
+
+  # export results
+  def export
+    ret = IO.readlines(File.join(job_dir, "marcoil" + @@export_ext)).join
+  end
+
+
+
+
+ # attr_reader :psipred, :coils, :alignment,  :pssm
+
+
+  def before_results(controller_params)
+
+    @problistname = File.join(job_dir,"ProbList")
+    @basename = File.join(job_dir, jobid)
+    @psipred = nil
+    @coils = nil
+    @alignment = nil
+    @pssm = false
+
+    if (File.exists?(@problistname +"PSSM"))
+ 	@pssm = true
+    end
+    if (File.exists?(@basename + "_psipred.png"))
+    	@psipred = true
+    end
+
+    if (File.exists?(@basename + "_ncoils.png"))
+    	@coils = true
+    end
+
+    if (File.exists?(@basename + ".alignment.psi") && !File.zero?(@basename + ".alignment.psi"))
+      @alignment = ""
+      res = IO.readlines(@basename + ".alignment.psi")
+      res.each do |line|
+        if (line =~ /\S+\s+(\S+)/)
+          @alignment += $1 + "\n"
+        end
+      end
+
+      @alignment.gsub!(/([WYF]+)/, '<span style="background-color: #00a000;">\1</span>')
+      @alignment.gsub!(/(C+)/, '<span style="background-color: #ffff00;">\1</span>')
+      @alignment.gsub!(/([DE]+)/, '<span style="background-color: #c080ff;">\1</span>')
+      @alignment.gsub!(/([LIVM]+)/, '<span style="background-color: #00ff00;">\1</span>')
+      @alignment.gsub!(/([KR]+)/, '<span style="background-color: #ff0000;">\1</span>')
+      @alignment.gsub!(/([QN]+)/, '<span style="background-color: #ffc0ff;">\1</span>')
+      @alignment.gsub!(/(H+)/, '<span style="background-color: #d00000;">\1</span>')
+      @alignment.gsub!(/(P+)/, '<span style="background-color: #808080;">\1</span>')
+      @alignment.gsub!(/(G+)/, '<span style="background-color: #ffd070;">\1</span>')
+    end
+    
+  end
+  
+end
