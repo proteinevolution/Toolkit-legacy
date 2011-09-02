625faeedd51b7a6ef35800ce380dcdb5332e6fa9
Marialke <marialke@genzentrum.lmu.de>
2011-09-02 11:00:41 +0200

    Ticket #124 Bfit not working on any toolkit
    Fixed the perform method to use the local python installation incl
    Scipy and numpy and local libgfortran.so.2

--- app/models/bfit/bfit_action.rb
+++ app/models/bfit/bfit_action.rb
@@ -57,8 +57,10 @@ class BfitAction < Action
   def perform
     @commands << "export PYTHONPATH=#{SAMCC}"
     if @ensemble == 'T'
+      #@commands << "#{PYTHON}/python #{BFIT}/bFit.py -e #{@pdb_file1} -l #{@model} -g #{@optimization} -w -p -o #{@outpath}"
       @commands << "#{PYTHON}/python #{BFIT}/bFit.py -e #{@pdb_file1} -l #{@model} -g #{@optimization} -w -p -o #{@outpath}"
     else
+      #@commands << "#{PYTHON}/python #{BFIT}/bFit.py -s #{@pdb_file1} #{@pdb_file2} -l #{@model} -g #{@optimization} -w -p -o #{@outpath}"
       @commands << "#{PYTHON}/python #{BFIT}/bFit.py -s #{@pdb_file1} #{@pdb_file2} -l #{@model} -g #{@optimization} -w -p -o #{@outpath}"
     end
     logger.debug "Commands:\n"+@commands.join("\n")
