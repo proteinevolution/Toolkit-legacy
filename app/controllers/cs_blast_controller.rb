714939beb626ea844bb3230a801009eb141e8950
Marialke <marialke@genzentrum.lmu.de>
2012-11-05 09:30:51 +0100

    Added Alignment Viewer Forwarding and T-Coffee forwarding to cs-Blast

--- app/controllers/cs_blast_controller.rb
+++ app/controllers/cs_blast_controller.rb
@@ -38,7 +38,8 @@ class CsBlastController < ToolController
     #   -seqs: use sequence by hsp or complete sequence
     
     # fill the possible forwarding parameters to cs_blast, here additional forwarding parameters can be added
-    @fw_values = [fw_to_tool_url('cs_blast', 'blammer'), 
+    @fw_values = [fw_to_tool_url('cs_blast', 'alnviz')+ "&fw_mode=alignment", 
+                  fw_to_tool_url('cs_blast', 'blammer'), 
                   fw_to_tool_url('cs_blast', 'blastclust') + "&fw_mode=seqs", 
                   fw_to_tool_url('cs_blast', 'clans') + "&fw_mode=seqs", 
                   fw_to_tool_url('cs_blast', 'clustalw') + "&fw_mode=seqs", 
@@ -57,9 +58,11 @@ class CsBlastController < ToolController
                   fw_to_tool_url('cs_blast', 'psi_blast') + "&fw_mode=alignment",
                   fw_to_tool_url('cs_blast', 'quick2_d') + "&fw_mode=alignment",                   
                   fw_to_tool_url('cs_blast', 'reformat') + "&fw_mode=alignment",                   
-                  fw_to_tool_url('cs_blast', 'repper') + "&fw_mode=alignment"]
+                  fw_to_tool_url('cs_blast', 'repper') + "&fw_mode=alignment",
+                  fw_to_tool_url('cs_blast', 't_coffee') + "&fw_mode=seqs"]
     # labels are added to the tools               
-    @fw_labels = [tool_title('blammer'),  tool_title('blastclust'), tool_title('clans'), 
+    @fw_labels = [tool_title('alnviz'),tool_title('blammer'),  
+                  tool_title('blastclust'), tool_title('clans'), 
                   tool_title('clustalw'), tool_title('cs_blast'), 
 		              tool_title('seq2gi'),   tool_title('frpred'),
                   tool_title('hhalign'),
@@ -67,7 +70,8 @@ class CsBlastController < ToolController
                   tool_title('hhsenser'), tool_title('kalign'),     tool_title('mafft'), 
                   tool_title('muscle'),   tool_title('patsearch'),
 		              tool_title('probcons'), tool_title('psi_blast'),
-		              tool_title('quick2_d'), tool_title('reformat'),   tool_title('repper')]
+		              tool_title('quick2_d'), tool_title('reformat'),   
+                  tool_title('repper'), tool_title('t_coffee')]
                  
 # Test if current CSblast Job is a uniprot Job and remove Blammer tool from @fw_values and @fw_label
    if(@job.is_uniprot == 1)
