dea6fa872b807b03f6f84a3fad9705a77acbe8cd
Marialke <marialke@genzentrum.lmu.de>
2012-11-21 16:04:06 +0100

    Added Forwarding to hhrep,missed forwarding method

--- app/models/hhrep/hhrep_forward_action.rb
+++ app/models/hhrep/hhrep_forward_action.rb
@@ -1,5 +1,6 @@
 class HhrepForwardAction < Action
   def run
+        logger.debug "L3 :Running  "
     self.status = STATUS_DONE
     self.save!
     job.update_status
@@ -9,13 +10,21 @@ class HhrepForwardAction < Action
     logger.debug "In forward params"
     logger.debug "#{job.job_dir}"
     filename = File.join(job.job_dir, 'result_textbox_to_file')
+     mode = params['fw_mode']
+    informat = 'fas'
+    inputmode = "alignment"
+     if (!mode.nil? && mode == "sequence")
+      inputmode = "sequence"
+    end
+    
+    logger.debug "L19 :#{mode}  "
     res = ""    
     if (File.exists?(filename))     
     	res = IO.readlines(filename).join
     end
     res.gsub!(/>ss_pred\s*\n(.*\n)*?>/, '>')
     res.gsub!(/>ss_conf\s*\n(.*\n)*?>/, '>')      
-    {'sequence_input' => res}
+    {'sequence_input' => res, 'inputmode' => inputmode }
   end
 end
 
