0120d061a032712668276b9fd2fa5c2b9b04f867
Marialke <marialke@genzentrum.lmu.de>
2012-11-21 16:32:13 +0100

    Added Forwarding of Alignments for HHrepid

--- app/models/hhrepid/hhrepid_forward_action.rb
+++ app/models/hhrepid/hhrepid_forward_action.rb
@@ -7,13 +7,24 @@ class HhrepidForwardAction < Action
   
   def forward_params
     filename = File.join(job.job_dir, 'result_textbox_to_file')
-    res = ""    
+    res = ""  
+    mode = params['fw_mode']
+    informat = 'fas'
+    inputmode = "alignment"
+    
+     if (!mode.nil? && mode == "sequence")
+      inputmode = "sequence"
+    end
     if (File.exists?(filename))     
     	res = IO.readlines(filename).join
-    end
+   end
+   if (!mode.nil? && mode == "alignment")
+      inputmode = "alignment"
+    end 
     res.gsub!(/>ss_pred\s*\n(.*\n)*?>/, '>')
     res.gsub!(/>ss_conf\s*\n(.*\n)*?>/, '>')      
-    {'sequence_input' => res}
+    {'sequence_input' => res, 'inputmode' => inputmode }
+
   end
 end
 
