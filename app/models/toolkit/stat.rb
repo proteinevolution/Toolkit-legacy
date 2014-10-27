b707e478af566ac260bd7471aaf4f008b4fbe6fd
Klaus Faidt <Klaus.Faidt@tuebingen.mpg.de>
2014-10-27 10:46:31 +0100

    More readable duration representation
    In the tool statistics page, times were given in seconds. That requiered an explanation, that the number is in seconds and large values were not easily readable.
    Now durations are splitted in d(ays), h(ours), m(inutes) and s(econds).

--- app/models/toolkit/stat.rb
+++ app/models/toolkit/stat.rb
@@ -17,4 +17,27 @@ class Stat < ActiveRecord::Base
     end
   end
 
+  def self.time2string(time, count)
+    return "" unless count > 0
+    s = time / count
+    return "0s" unless s > 0
+
+    m = s / 60
+    s = s % 60
+    h = m / 60
+    m = m % 60
+    d = h / 24
+    h = h % 24
+
+    result = ""
+    result += "#{d}d" if d > 0
+    result += "#{h}h" if h > 0
+    result += "#{m}m" if m > 0
+    result += "#{s}s" if s > 0
+  end
+
+  def time2string()
+    Stat.time2string(jobs_time, jobs_count)
+  end
+
 end
