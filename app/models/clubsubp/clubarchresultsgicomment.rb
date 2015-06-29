0846b4d679bf96807a2bc379a15581ffdc5ff716
Klaus Faidt <kfaidt@tuebingen.mpg.de>
2015-06-29 08:12:06 +0200

    still more connection problems with Clubsub-P

--- app/models/clubsubp/clubarchresultsgicomment.rb
+++ app/models/clubsubp/clubarchresultsgicomment.rb
@@ -1,6 +1,9 @@
+require "protected_sql.rb"
 class Clubarchresultsgicomment < ActiveRecord::Base
     establish_connection "clubsubp"
     has_and_belongs_to_many :clubarchgipredictions
     has_and_belongs_to_many :clubproteinheaderinfos
+
+    include ProtectedSql
 end
 
--- app/models/clubsubp/clubgiprediction.rb
+++ app/models/clubsubp/clubgiprediction.rb
@@ -1,7 +1,11 @@
+require "protected_sql.rb"
+
 class Clubgiprediction < ActiveRecord::Base
     establish_connection "clubsubp"
     has_and_belongs_to_many :clubclstpredictions
     has_and_belongs_to_many :clubproteinheaderinfos
+
+    include ProtectedSql
 
 end
 
