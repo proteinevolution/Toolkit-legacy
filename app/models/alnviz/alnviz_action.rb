52c0d1632adddf35c50072ad17448be1a119159a
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2015-11-13 04:15:42 +0100

    tricked the biojsmsa parser for use cases without gi numbers with a ruby hack

--- app/models/alnviz/alnviz_action.rb
+++ app/models/alnviz/alnviz_action.rb
@@ -16,11 +16,43 @@ class AlnvizAction < Action
   validates_shell_params(:jobid, :mail, {:on => :create})
 
   def before_perform
-    @basename = File.join(job.job_dir, job.jobid)
+    @basename = File.join(job.job_dir, job.jobid)    
     @outfile = @basename+".out"
+
+   
+  
     params_to_file(@outfile, 'sequence_input', 'sequence_file')
+
+
+
+
+    #Author: Seung-Zin Nam
+    #this code is a hack to provide fake gi numbers for biojs msa to handle custom ids
+    #TODO: another hack to do the renumbering which happens in the backbone.js interface on the server!
+    res = IO.readlines(sequence_file)
+    
+    out = File.new(@outfile, "w+")
+    delim = '>'
+    
+    File.readlines(sequence_file).each do |line|
+      if ((line.include? '>') && !(line.include? '>gi|'))
+        then
+        out.write(line.split(delim).join(delim + 'gi|'))
+      else
+        out.write(line)
+      end
+    end
+    out.close
+
+
+
+
+
+
     @informat = params['informat'] ? params['informat'] : 'fas'
     reformat(@informat, "clu", @outfile)
+
+   
 
     @commands = []
  
@@ -28,7 +60,10 @@ class AlnvizAction < Action
 
   def perform
     params_dump
-    
+
+
+
+    @commands << "cp sequence_file #{@basename}.in"
     @commands << "#{HH}/reformat.pl clu fas #{@basename}.out #{@basename}.align"
     @commands << "#{HH}/reformat.pl clu a3m #{@basename}.out #{@basename}.a3m"
     @commands << "#{HH}/reformat.pl clu fas #{@basename}.out #{@basename}.ralign -M first -r"
