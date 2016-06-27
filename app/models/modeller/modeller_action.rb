320dd8f910b36be3557a278ef418fa8c3f1b37c9
Vikram Alva <vikram.alva@tuebingen.mpg.de>
2016-06-27 17:48:50 +0200

    Updated Modeller to use the PDB70 DB of HHpred
    Until now, Modeller used the pdb/all database together
    with the HHpred HMMs. Now, for Modeller jobs forwarded
    by HHpred, the HHpred PDB70 database is searched.

--- app/models/modeller/modeller_action.rb
+++ app/models/modeller/modeller_action.rb
@@ -20,109 +20,110 @@ class ModellerAction < Action
                                                     :on => :create })
 
   validates_jobid(:jobid)
-  
+
   validates_email(:mail)
-  
+
   validates_presence_of(:modeller_key, {:on => :create})
-  
+
   validates_shell_params(:jobid, :mail, {:on => :create})
-  
+
   # Put action initialisation code in here
   def before_perform
-    
+
     @basename = File.join(job.job_dir, job.jobid)
     @seqfile = @basename + ".prepare"
     @infile = @basename + ".in"
     params_to_file(@seqfile, 'sequence_input', 'sequence_file')
-    
+
     @ownpdbfiles = ['own_pdb_file1','own_pdb_file2','own_pdb_file3','own_pdb_file4','own_pdb_file5']
     @ownpdbnames =  ['own_pdb_name1','own_pdb_name2','own_pdb_name3','own_pdb_name4','own_pdb_name5']
     @ownpdbfiles.each_index  do |i|
       ownpdbfile = File.join(job.job_dir, "#{params[@ownpdbnames[i]]}.pdb")
       params_to_file(ownpdbfile, @ownpdbfiles[i])
     end
-    
+
     @commands = []
-    
+
     @format = params["informat"].nil? ? 'fas' : params["informat"]
-    
+
     @seq_name = "#{job.jobid}_temp";
     @local_dir = "/tmp"
-    
+
     @modeller_key = params['modeller_key']
     @need_key = params['need_key']
-    
+
     if (@need_key == "false")
       @modeller_key = "MODELIRANJE"
     end
-    
+
   end
-  
+
 
   # Optional:
   # Put action initialization code that should be executed on forward here
   def before_perform_on_forward
     pjob = job.parent
   end
-  
-  
+
   # Put action code in here
   def perform
-    
-    if job.parent.nil? 
-      
+
+    if job.parent.nil?
+
       if (@format != "pir")
-        reformat(@format, "fas", @seqfile)			
-      
+        reformat(@format, "fas", @seqfile)
+
         # convert alignment in fasta-format
         command = "export TK_ROOT=#{TOOLKIT_ROOT}; #{MODELLER}/pir_converter.pl -i #{@seqfile} -o #{@infile} -fas -tmp #{job.job_dir} >> #{@basename + '.log_pir_converter'} 2>&1"
         logger.debug "Command: #{command}"
         system(command)
-        
+
       else
 
         command = "cp #{@seqfile} #{@infile}"
         system(command)
-        
+
       end
 
-      # change sequence name	
+      # change sequence name
       i = nil
       lines = IO.readlines(@infile)
       lines.each do |line|
         if line =~ /^sequence:/
           i = lines.index(line)
         end
-        line.sub!(/>P1;(\S\S\S\S)_\S/, '>P1;\1')
-        line.sub!(/structureX:\s*(\S\S\S\S)_\S:/, 'structureX:\1:')
+	# do not change sequence name; let modeller decide what database to pick
+        #line.sub!(/>P1;(\S\S\S\S)_\S/, '>P1;\1')
+        #line.sub!(/structureX:\s*(\S\S\S\S)_\S:/, 'structureX:\1:')
       end
       if i.nil? then raise "ERROR! Wrong format!" end
-      
+
       lines[i-1] = ">P1;#{@seq_name}\n"
       lines[i].gsub!(/^sequence:.*?:(.*)$/, "sequence:#{@seq_name}:"+'\1')
-      
+
       File.open(@infile, 'w') do |file|
         file.write(lines.join(''))
       end
     else
-      
+
       input = IO.read(@seqfile)
-      
+
       # special character (Ascii Code 13) rauszufiltern
       input.gsub!(/\13/, '')
-      
+
       # remove chain identifiers
-      input.gsub!(/>P1;(\S+)_\S\s*$/, '>P1;\1')
-      input.gsub!(/structureX:\s*(\S+)_\S:/, 'structureX:\1:')
+      # do not change sequence name; let modeller decide what database to pick
+      # input.gsub!(/>P1;(\S+)_\S\s*$/, '>P1;\1')
+      # input.gsub!(/structureX:\s*(\S+)_\S:/, 'structureX:\1:')
       # change sequence name
       input.sub!(/^[^\n]*/, ">P1;#{@seq_name}")
-      
+
       File.open(@infile, 'w') do |file|
         file.write(input)
       end
-      
+
     end
-    
+
     # replace special character (Selenocystein U)
     lines = IO.readlines(@infile)
     lines.each do |line|
@@ -153,12 +154,13 @@ class ModellerAction < Action
     end
 
     @hhpred_dbs = ""
-    if job.parent && job.parent.parent && job.parent.parent.class.to_s == 'HhpredJob'
-      @hhpred_dbs = job.parent.parent.actions.first.params['hhpred_dbs'].nil? ? "" : job.parent.parent.actions.first.params['hhpred_dbs']
-      if @hhpred_dbs.kind_of?(Array) then @hhpred_dbs = @hhpred_dbs.join(':') end
-    end
-    
-    # write the py-file		
+    # Ignore parent job; always search hhpred_dbs
+    #if job.parent && job.parent.parent && job.parent.parent.class.to_s == 'HhpredJob'
+     # @hhpred_dbs = job.parent.parent.actions.first.params['hhpred_dbs'].nil? ? "" : job.parent.parent.actions.first.params['hhpred_dbs']
+      #if @hhpred_dbs.kind_of?(Array) then @hhpred_dbs = @hhpred_dbs.join(':') end
+    #end
+
+    # write the py-file
     modeller_script = @basename + ".py"
     File.open(modeller_script, 'w') do |file|
       file.write("# Homology modeling by the automodel class\n")
@@ -172,7 +174,7 @@ class ModellerAction < Action
       if !job.parent.nil?
          parent_dir = ":#{job.parent.job_dir}"
       end
-      file.write("env.io.atom_files_directory = '#{DATABASES}/pdb/all:#{DATABASES}/hhomp/pdb:#{@hhpred_dbs}:#{job.job_dir}#{parent_dir}'\n")
+      file.write("env.io.atom_files_directory = '#{DATABASES}/pdb/all:#{DATABASES}/hhomp/pdb:#{DATABASES}/hhpred/pdb70:#{job.job_dir}#{parent_dir}'\n")
 
       file.write("a = automodel(env,\n")
       file.write("              alnfile  = '#{@infile}',    # alignment filename\n")
