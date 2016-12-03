e985a26f11106ac6a41a366638c1740888a758c0
lukas <lukas.zimmermann@tuebingen.mpg.de>
2016-12-03 20:23:04 +0100

    Remove GI numbers from Alnviz example sequence. Increase size of tool's input textarea.

--- app/controllers/alnviz_controller.rb
+++ app/controllers/alnviz_controller.rb
@@ -4,6 +4,4 @@ class AlnvizController < ToolController
 		@informat_values = ['fas', 'clu', 'sto', 'a2m', 'a3m', 'emb', 'meg', 'msf', 'pir', 'tre']
 		@informat_labels = ['FASTA', 'CLUSTAL', 'Stockholm', 'A2M', 'A3M', 'EMBL', 'MEGA', 'GCG/MSF', 'PIR/NBRF', 'TREECON']
 	end
-
-  
 end
