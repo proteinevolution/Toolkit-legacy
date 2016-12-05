a9af9bbc52e9670fd0d09b8a3bd677ea3770f9bf
lukas <lukas.zimmermann@tuebingen.mpg.de>
2016-12-05 14:38:15 +0100

    HHblits result file (hhr) is now parsed to be supplemented with links to Uniprot/trembl

--- app/models/hhblits/hhblits_action.rb
+++ app/models/hhblits/hhblits_action.rb
@@ -3,7 +3,8 @@ class HhblitsAction < Action
   #HHSUITE = File.join(BIOPROGS, 'hhsuite/bin')
   #HHSUITELIB = File.join(BIOPROGS, 'hhsuite/lib/hh/scripts')
   RUBY_UTILS = File.join(BIOPROGS, 'ruby')
-  
+  HELPER = File.join(BIOPROGS, "helper")
+
   if LOCATION == "Munich" && LINUX == 'SL6'
     PERL = "perl "+File.join(BIOPROGS, 'perl')
    # HH   = "perl "+File.join(BIOPROGS, 'hhpred')
@@ -134,6 +135,8 @@ class HhblitsAction < Action
     # -qhhm parameter was removed here 
     @commands << "hhblits -cpu 8 -v #{@v} -i #{@infile} -d #{@db}  -o #{@outfile} -oa3m #{@a3m_outfile}  -e #{@E_hhblits} -n #{@maxit} -p #{@Pmin} -Z #{@max_lines} -z 1 -b 1 -B #{@max_lines} -seq #{@max_seqs} -aliw #{@aliwidth} -#{@ali_mode} #{@realign} #{@mact} #{@filter} #{@cov_min} 1>> #{job.statuslog_path} 2>> #{job.statuslog_path}; echo 'Finished search'"
 
+
+
     @commands << "addss.pl #{@a3m_outfile}"
 
     prepare_fasta_hhviz_histograms_etc    
@@ -146,6 +149,9 @@ class HhblitsAction < Action
     @commands << "source #{UNSETENV}" 
     
 
+    # Parse outfile
+    @commands << "#{HELPER}/blast-parser.pl -i #{@outfile} --add-links > outfile_temp.hhr"
+    @commands << "mv outfile_temp.hhr #{@outfile}"
     logger.debug "Commands:\n"+@commands.join("\n")
     #queue.submit(@commands, true, {'cpus' => '4', 'queue' => QUEUES[:hhblits]})
     # declare as much cpus as are used by the commands
